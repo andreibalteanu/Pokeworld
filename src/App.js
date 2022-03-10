f26c741e302431bcbc5a778eab42bdf01c7782f2
Balteanu Andrei <balteanu_andrei98@yahoo.com>
2022-03-10 03:45:41 +0200

    passed json data as props to components + more styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
 import Card from "./Card.js";
+import PokemonData from "./data.json";
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
+      <input
+        placeholder="Search pokemon name, number or type..."
+        class="searchBar"
+      ></input>
       <div className="container-of-cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {PokemonData.map((pokemon) => {
+          return (
+            <Card
+              key={pokemon.id}
+              name={
+                pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+              }
+              id={pokemon.id}
+              types={pokemon.types}
+              image={pokemon.sprites.other.official_artwork.front_default}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,29 @@
 import "./Card.css";
 
-function Card() {
+function Card({ name, id, types, image }) {
+  function CorrectId(number) {
+    if (number > 0 && number < 10) return "00" + number;
+    else if (number > 9 && number < 100) return "0" + number;
+    else if (number > 99 && number < 1000) return number;
+  }
   return (
     <div className="Card">
       <div className="top-card">
-        <h3>Name</h3>
-        <h3>Index</h3>
+        <h3>{name}</h3>
+        <h3 className={id}>#{CorrectId(id)}</h3>
       </div>
       <div className="bottom-card">
-        <h3>Type</h3>
-        <h3>Image</h3>
+        <div className="type-list">
+          {types.map((pokemon) => {
+            return (
+              <h3 className={pokemon.type.name}>
+                {pokemon.type.name.charAt(0).toUpperCase() +
+                  pokemon.type.name.slice(1)}
+              </h3>
+            );
+          })}
+        </div>
+        <img className="card-image" alt="Pokemon" src={image} />
       </div>
     </div>
   );
