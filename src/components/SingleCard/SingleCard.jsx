99e4e5edc095c5af8e716ea542b757791d7b8fdd
Andrei BALTEANU <andrei.balteanu@falcontrading.ro>
2022-04-07 15:19:43 +0300

    finished Card Game feature

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./SingleCard.css";
 
-function SingleCard({ card, backCard }) {
-  const handleClick = () => {};
+function SingleCard({ card, backCard, handleChoice, flipped }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
   return (
     <div className="card">
-      <img src={card.src} className="front" alt="card front" />
-      <img
-        src={backCard}
-        className="back"
-        alt="card back"
-        onClick={handleClick}
-      />
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} className="front" alt="card front" />
+        <img
+          src={backCard}
+          className="back"
+          alt="card back"
+          onClick={handleClick}
+        />
+      </div>
     </div>
   );
 }
--- src/pages/CardGame/CardGame.jsx
+++ src/pages/CardGame/CardGame.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
 import "./CardGame.css";
 import { Button, Flex } from "@chakra-ui/react";
 import TopPage from "components/TopPage/TopPage";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SingleCard from "components/SingleCard/SingleCard";
 import blastoise from "../../images/blastoise.png";
 import gardevoir from "../../images/gardevoir.png";
@@ -10,33 +12,65 @@ import raichu from "../../images/raichu.png";
 import backCard from "../../images/pokemon-card-back.jpeg";
 
 const cardImages = [
-  { src: blastoise },
-  { src: gardevoir },
-  { src: mewtwo },
-  { src: raichu },
+  { src: blastoise, matched: false },
+  { src: gardevoir, matched: false },
+  { src: mewtwo, matched: false },
+  { src: raichu, matched: false },
 ];
 
 function CardGame() {
   const [cards, setCards] = useState([]);
   const [firstChoice, setFirstChoice] = useState(null);
   const [secondChoice, setSecondChoice] = useState(null);
+  const [numberOfMatches, setNumberOfMatches] = useState(0);
   const shuffleCards = () => {
+    setNumberOfMatches(0);
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
     setCards(shuffledCards);
   };
 
+  const handleChoice = (card) => {
+    // eslint-disable-next-line no-unused-expressions
+    firstChoice ? setSecondChoice(card) : setFirstChoice(card);
+  };
+
+  const resetChoices = () => {
+    setFirstChoice(null);
+    setSecondChoice(null);
+  };
+  useEffect(() => {
+    if (firstChoice && secondChoice) {
+      if (firstChoice.src === secondChoice.src) {
+        setNumberOfMatches(numberOfMatches + 1);
+        setCards((prevCards) =>
+          prevCards.map((card) => {
+            if (card.src === firstChoice.src) {
+              return { ...card, matched: true };
+            }
+            return card;
+          })
+        );
+        resetChoices();
+      } else {
+        setTimeout(() => resetChoices(), 800);
+      }
+    }
+  }, [firstChoice, secondChoice]);
+
   return (
     <Flex direction="column" align="center">
       <TopPage path="/" title="Memory Card Game" />
-      <Button
-        colorScheme="blue"
-        className="option-button"
-        onClick={shuffleCards}
-      >
-        New Game
-      </Button>
+      <Flex align="center">
+        {numberOfMatches === 4 && (
+          <div className="success-message">SUCCESS!!</div>
+        )}
+        <Button colorScheme="blue" onClick={shuffleCards}>
+          New Game
+        </Button>
+      </Flex>
+
       <Flex
         className="card-container"
         wrap="wrap"
@@ -45,7 +79,15 @@ function CardGame() {
         align="center"
       >
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} backCard={backCard} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            backCard={backCard}
+            handleChoice={handleChoice}
+            flipped={
+              card === firstChoice || card === secondChoice || card.matched
+            }
+          />
         ))}
       </Flex>
     </Flex>
